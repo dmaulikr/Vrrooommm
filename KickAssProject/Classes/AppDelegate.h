78bd5f4ef9ebda78e91f243afb04a1a9348024a7
ganlaw <gannonlawlor@gmail.com>
2014-03-24 10:23:21 -0230

    Initial Commit

---
+++ KickAssProject/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  KickAssProject
+//
+//  Created by Gannon lawlor on 2014-03-24.
+//  Copyright Comp4768 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ KickAssProject/Classes/HelloWorldScene.h
@@ -0,0 +1,27 @@
+//
+//  HelloWorldScene.h
+//  KickAssProject
+//
+//  Created by Gannon lawlor on 2014-03-24.
+//  Copyright Comp4768 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+// Importing cocos2d.h and cocos2d-ui.h, will import anything you need to start using Cocos2D v3
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+/**
+ *  The main scene
+ */
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
++ (HelloWorldScene *)scene;
+- (id)init;
+
+// -----------------------------------------------------------------------
+@end
